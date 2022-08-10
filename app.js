43bfbac50882686cdaea85c5c78a07fda682f158
Asher Ayub <99917058+asherayub@users.noreply.github.com>
2022-08-11 00:28:46 +0100

    add score functionality

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ start.addEventListener("click", () => {
 
 // ?    restart button
 document.querySelector(".restart").addEventListener("click", () => {
+    //?     reset everything
+    score = 0
+    scoreSpanEl.textContent =  "0"
+    clickedBtnArray = []
     document.querySelector(".menu-ul").classList.remove("menu-ul-collapse")
     numberClicked.forEach((number) => {
         number.textContent = ""
@@ -22,26 +26,39 @@ menuToggle.addEventListener("click", () => {
 })
 
 
-
+const scoreSpanEl = document.querySelector("#score-span")
+let score = 0
 // ?    number clicked logic
 const numberClicked = document.querySelectorAll('.number')
+let clickedBtnArray = []
+scoreSpanEl.textContent = score
 numberClicked.forEach((number) => {
     number.addEventListener("click", (e) => {
+        if (clickedBtnArray.length > 1) {
+            clickedBtnArray = []
+        }
         if (number.textContent == "") {
             number.textContent = randomNumber()
             number.style.background = "orangered"
-            let numberClickOne = e.target.textContent
-            console.log("click " + numberClickOne);    
+            // let numberClickOne = e.target.textContent
+            // console.log("click " + numberClickOne);
+            clickedBtnArray.push(e.target.textContent)
+            console.log(clickedBtnArray);
+            if (clickedBtnArray.length == 2) {
+                if (clickedBtnArray[clickedBtnArray.length - 2] == clickedBtnArray[clickedBtnArray.length - 1]) {
+                    console.log("equal");
+                    score += 10
+                }
+
+            }
         }
-        if(number.textContent == number.textContent){
-            console.log("won");
-        }
+        scoreSpanEl.textContent = score
     })
 })
 
 
 // ?    generates random number each time it is called
 function randomNumber() {
-    let randomDigit = Math.floor(Math.random() * 8 + 1)
+    let randomDigit = Math.floor(Math.random() * 5 + 1)
     return randomDigit
 }
